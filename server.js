6b597747321134f373533143e8fbd91296f39576
ANDREW SEGAL <segal.a@hotmail.com>
2018-05-02 14:45:45 -0400

    added request method reponses'

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ var server = http.createServer(function(request, response){
   response.writeHead(200, {'Content-Type': "text/html"});
   response.end('<strong>Hello</strong> World')
 } else if (request.url === '/goodbye') {
+  if (request.method === 'GET'){
   response.writeHead(200, {'Content-Type': "text/html"});
   response.end('<strong>Goodbye</strong> World')
+} else if (request.method === 'POST') {
+  response.writeHead(200, {'Content-Type': "text/html"});
+  response.end('Posting <strong>Goodbye</strong> Cruel World')
+  }
 }
 });
 
