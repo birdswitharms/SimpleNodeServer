19a779732edde4ac9d4997f181fedaf7c0298d5a
ANDREW SEGAL <segal.a@hotmail.com>
2018-05-02 14:43:05 -0400

    responde depending on path

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response){
+
+  if (request.url === '/'){
   response.writeHead(200, {'Content-Type': "text/html"});
-  response.end('<strong>Hello World</strong>')
+  response.end('<strong>Hello</strong> World')
+} else if (request.url === '/goodbye') {
+  response.writeHead(200, {'Content-Type': "text/html"});
+  response.end('<strong>Goodbye</strong> World')
+}
 });
 
 server.listen(3000)
